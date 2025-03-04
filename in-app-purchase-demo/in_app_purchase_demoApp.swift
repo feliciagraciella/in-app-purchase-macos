91635d96bd83403491ff039129a03ab7374d38b0
Felicia Graciella <75228967+feliciagraciella@users.noreply.github.com>
2025-03-04 18:28:26 +0700

    Initial Commit

---
+++ in-app-purchase-demo/in_app_purchase_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  in_app_purchase_demoApp.swift
+//  in-app-purchase-demo
+//
+//  Created by Felicia Graciella on 04/03/25.
+//
+
+import SwiftUI
+
+@main
+struct in_app_purchase_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ in-app-purchase-demoTests/in_app_purchase_demoTests.swift
@@ -0,0 +1,17 @@
+//
+//  in_app_purchase_demoTests.swift
+//  in-app-purchase-demoTests
+//
+//  Created by Felicia Graciella on 04/03/25.
+//
+
+import Testing
+@testable import in_app_purchase_demo
+
+struct in_app_purchase_demoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
